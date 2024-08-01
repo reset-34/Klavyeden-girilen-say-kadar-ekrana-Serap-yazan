4f96d1a9cf6dda9c44e712d582d0b6d05349c388
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:04:24 +0300

    Add project files.

---
+++ Klavyeden girilen sayı kadar ekrana Serap yazan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_girilen_sayı_kadar_ekrana_Serap_yazan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı girin");
+            int sayi = Int32.Parse(Console.ReadLine());
+
+            for (int i= 0;i<sayi;i++)
+            {
+                Console.WriteLine("Serap");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
